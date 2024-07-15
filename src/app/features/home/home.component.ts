f59231d1924384f1120ae384f850d2588e6ca95a
abdhulla-k <abdhullak149@gmail.com>
2024-07-15 17:10:17 +0530

    installed tailwind and prime ng and configured both

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,90 @@
 import { Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { BadgeModule } from 'primeng/badge';
+import { RippleModule } from 'primeng/ripple';
+
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [ButtonModule, TieredMenuModule, BadgeModule, RippleModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  items: any;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'File',
+                icon: 'pi pi-file',
+                items: [
+                    {
+                        label: 'New',
+                        icon: 'pi pi-plus',
+                        items: [
+                            {
+                                label: 'Document',
+                                icon: 'pi pi-file'
+                            },
+                            {
+                                label: 'Image',
+                                icon: 'pi pi-image'
+                            },
+                            {
+                                label: 'Video',
+                                icon: 'pi pi-video'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Open',
+                        icon: 'pi pi-folder-open'
+                    },
+                    {
+                        label: 'Print',
+                        icon: 'pi pi-print'
+                    }
+                ]
+            },
+            {
+                label: 'Edit',
+                icon: 'pi pi-file-edit',
+                items: [
+                    {
+                        label: 'Copy',
+                        icon: 'pi pi-copy'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-times'
+                    }
+                ]
+            },
+            {
+                label: 'Search',
+                icon: 'pi pi-search'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'Share',
+                icon: 'pi pi-share-alt',
+                items: [
+                    {
+                        label: 'Slack',
+                        icon: 'pi pi-slack'
+                    },
+                    {
+                        label: 'Whatsapp',
+                        icon: 'pi pi-whatsapp'
+                    }
+                ]
+            }
+        ]
+    }
 
 }

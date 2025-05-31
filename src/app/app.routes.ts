62a3b02fdda8a0fd4cf0007da2b7fcbc8e9d2297
abdhulla-k <abdhullak149@gmail.com>
2025-05-31 17:21:35 +0530

    added title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ export const routes: Routes = [
     },
     {
         path: '',
-        component: HomeComponent
+        title: "Abdhulla K | Software Engineer",
+        component: HomeComponent,
     },
 ];
